0f256c5ed8a017a4349ce402dbb76fb08f8dd6b0
J. Scott Thompson <59769815+jscott-thompson@users.noreply.github.com>
2020-07-06 20:35:18 -0400

    Add files via upload

---
+++ oakwood_city_budget_2020.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 20:03:59 2020
+
+@author: Scott
+"""
+#%%
+import matplotlib.pyplot as plt
+import pandas as pd
+import squarify
+
+#%%
+df = pd.read_csv("oakwood_city_budget_2020.csv")
+plt.figure(figsize=(10,10))
+squarify.plot(sizes=df['Percent'], label=df['Category'],alpha=.8,color=['#7fc97f',
+              '#beaed4',
+              '#fdc086',
+              '#ffff99',
+              '#386cb0',
+              '#f0027f',
+              '#bf5b17'])
+plt.axis('off');
+plt.title('Oakwood 2020 Budget')
+plt.show()
